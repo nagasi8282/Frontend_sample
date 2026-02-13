2711ca3aefbf414af2382f8602d722d109996dc2
nagasi8282 <nagasai_challapalli@gmail.com>
2026-02-13 10:51:45 +0530

    update

---
+++ arrmethods.js
@@ -0,0 +1,28 @@
+// let value = [10,20,30,40,50];
+// let result = value.map(num=>num*2);
+// console.log(result);
+
+// let value = [2,5,7,9,8];
+// let result = value.filter(num=>num%2==0);
+// console.log(result);
+
+// let value = [3,5,7,2,9];
+// let result = value.find(num=>num%3==0);
+// console.log(result);
+
+// let value = [100,200,300,400];
+// let result = value.reduce((totalValue, acc)=>{
+//     return totalValue + acc
+// },0);
+// console.log(result);
+
+// let value = [100,200,300,400,500]
+// let res = value.forEach(element => {
+//     console.log(element);
+// });
+
+// console.log("First");
+// console.log("Second");
+// console.log("Third");
+// console.log("Fourth");
+// console.log("fifth");
---
+++ callback.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    return a+b;
+}
+function sub(x,y){
+    console.log(x-y);
+}
+function div(i,j){
+    console.log(i/j);
+}
+
+add(10,20,sub);
